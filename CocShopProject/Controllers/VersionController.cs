99c87abbcfadeb5da55eb6b3b719061e63642ff8
thienmanhvo <benficacronaldo@gmail.com>
2019-09-27 11:05:38 +0700

    version

---
+++ CocShopProject/Controllers/VersionController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CocShop.WebAPi.Controllers
+{
+    public class VersionController : ControllerBase
+    {
+
+        public VersionController()
+        {
+        }
+
+        [HttpGet("/version")]
+        public string GetVersion()
+        {
+            return typeof(Program).Assembly.GetName().Version.ToString();
+        }
+    }
+}
