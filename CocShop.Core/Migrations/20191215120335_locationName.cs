dc3e2af71f4979083c64be6787e37470a13c4f76
thienmanhvo <benficacronaldo@gmail.com>
2019-12-15 21:29:00 +0700

    add location name

---
+++ CocShop.Core/Migrations/20191215120335_locationName.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CocShop.Data.Migrations
+{
+    public partial class locationName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Location_Name",
+                table: "Store",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Total_Store",
+                table: "Store",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Location_Name",
+                table: "Store");
+
+            migrationBuilder.DropColumn(
+                name: "Total_Store",
+                table: "Store");
+        }
+    }
+}
